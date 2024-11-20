dc42f511d3bc1471d9e1a9df646fff864f86eab2
ArsenMv <arsenmakkay@gmail.com>
2024-11-21 01:05:53 +0300

    Добавил в класс main 3 машины с моделями

--- src/main/java/ru/makkay/Main.java
+++ src/main/java/ru/makkay/Main.java
@@ -2,6 +2,13 @@ package ru.makkay;
 
 public class Main {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setCarModel("BMW");
+        Car car2 = new Car();
+        car2.setCarModel("LADA");
+        Car car3 = new Car();
+        car3.setCarModel("Mercedes");
 
+        System.out.println("Количество машин: " + Car.getObjectCount());
     }
 }
