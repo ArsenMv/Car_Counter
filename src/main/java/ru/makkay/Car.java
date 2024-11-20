e55f4a2b9268e18fd61a6029caf13172b314d49d
ArsenMv <arsenmakkay@gmail.com>
2024-11-21 01:00:41 +0300

    Добавил в класс Car счетчик объектов и модель машин

--- src/main/java/ru/makkay/Car.java
+++ src/main/java/ru/makkay/Car.java
@@ -1,4 +1,22 @@
 package ru.makkay;
 
 public class Car {
+    private static int objectCount = 0;
+    private String carModel;
+
+    public Car() {
+        objectCount++;
+    }
+
+    public static int getObjectCount() {
+        return objectCount;
+    }
+
+    public void setCarModel(String model) {
+        this.carModel = carModel;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
 }
